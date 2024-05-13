5ffdc916d3a44dbdae14c25520fb71938ba9fc8b
Жаладдинов Наркес Шаймуратович <ZhaladdinovN@lcoast.itstep>
2024-05-13 22:52:51 +0500

    practic2

--- Desktop/django/Lessonpractic47-49/New/Lesson/urls.py
+++ Desktop/django/Lessonpractic47-49/New/Lesson/urls.py
@@ -2,9 +2,15 @@ from . import views
 from django.urls import path
 
 urlpatterns = [
-    # path('todos/', views.TodoList.as_view(), name='todo_list'),
-    # path('todos/<int:pk>', views.TodolistDetail.as_view(), name='todo_detail'),
-    # path('todos/create', views.TodolistCreate.as_view(), name='todo_create'),
-    # path('todos/<int:pk>/update/', views.TodolistUpdate.as_view(), name='todo_update'),
-    # path('todos/<int:pk>/delete/', views.TodolistDelete.as_view(), name='todo_delete'),  
+    path('cars/', views.CarList.as_view(), name='car_list'),
+    path('cars/<int:pk>', views.CarDetail.as_view(), name='car_detail'),
+    path('cars/create', views.CarCreate.as_view(), name='car_create'),
+    path('cars/<int:pk>/update/', views.CarUpdate.as_view(), name='car_update'),
+    path('cars/<int:pk>/delete/', views.CarDelete.as_view(), name='car_delete'), 
+
+    path('categorys/', views.CategoryList.as_view(), name='category_list'),
+    path('categorys/<int:pk>', views.CategoryDetail.as_view(), name='category_detail'),
+    path('categorys/create', views.CategoryCreate.as_view(), name='category_create'),
+    path('categorys/<int:pk>/update/', views.CategoryUpdate.as_view(), name='category_update'),
+    path('categorys/<int:pk>/delete/', views.CategoryDelete.as_view(), name='category_delete'),  
 ]
--- Desktop/django/Lessonpractic47-49/New/Lesson/views.py
+++ Desktop/django/Lessonpractic47-49/New/Lesson/views.py
@@ -1,3 +1,59 @@
 from django.shortcuts import render
+from django.views.generic.list import ListView
+from django.views.generic.detail import DetailView
+from django.views.generic.edit import CreateView, UpdateView, DeleteView
+from django.urls import reverse_lazy
+
+from .models import Car, Category
 
 # Create your views here.
+
+class CarList(ListView):
+    model = Car
+    context_object_name = 'car_list'
+    paginate_by = 1
+
+class CarDetail(DetailView):
+    model = Car
+    context_object_name = 'car'
+
+class CarCreate(CreateView):
+    model = Car
+    fields = '__all__'
+    success_url = reverse_lazy('car_list')
+
+class CarUpdate(UpdateView):
+    model = Car
+    fields = '__all__'
+    success_url = reverse_lazy('car_list')
+
+class CarDelete(DeleteView):
+    model = Car
+    fields = '__all__'
+    success_url = reverse_lazy('car_list')
+    template_name = 'Lesson/car_form.html'
+
+class CategoryList(ListView):
+    model = Category
+    context_object_name = 'category_list'
+    paginate_by = 1
+
+class CategoryDetail(DetailView):
+    model = Category
+    context_object_name = 'category'
+
+class CategoryCreate(CreateView):
+    model = Category
+    fields = '__all__'
+    success_url = reverse_lazy('category_list')
+
+class CategoryUpdate(UpdateView):
+    model = Category
+    fields = '__all__'
+    success_url = reverse_lazy('category_list')
+
+class CategoryDelete(DeleteView):
+    model = Category
+    fields = '__all__'
+    success_url = reverse_lazy('category_list')
+    template_name = 'Lesson/category_form.html'
